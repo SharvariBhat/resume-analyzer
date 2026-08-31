94479ae4ad7a4f0d6d17c023674a9b2fd0462410
SharvariBhat <sharvarisiri@gmail.com>
2026-09-01 01:16:59 +0530

    update

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 async def home():
     return {"message": "Resume Analyzer API is running"}
+
+class ResumeInfo(BaseModel):
+    name: str
+    email: str
+    phone: str
+
+
+@app.post("/resume")
+async def create_resume(resume: ResumeInfo):
+    return resume
